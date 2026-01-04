82dcfc28fad9d4894f763dadc5818b7de3362e59
Clawz-123 <pukarbohara9@gmail.com>
2026-01-04 10:32:27 +0545

    Update: requiremnt file and gitignore file

--- Backend/accounts/permission.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsSuperUser (BasePermission):
-    def has_permission(self, request):
-        return bool(request.user.is_authenticated
-                    and request.user.role == "SuperAdmin")
